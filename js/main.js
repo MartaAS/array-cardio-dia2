6807f6d993ceddf3045be1758389c6d0cedc3bc4
usuario <marta.aguilar@kairos.com>
2018-04-27 13:18:55 +0200

    js code

--- js/main.js
+++ js/main.js
@@ -17,12 +17,43 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19?
+
+	const isAdult = people.some(function(person) {
+	const currentYear = (new Date()).getFullYear();
+	if(currentYear - person.year >= 19) {
+	return true;
+		}
+	});
+	console.log(isAdult);
+
 // Array.prototype.every() // is everyone 19?
+
+	const allAdults = people.every(person => ((new Date())
+	.getFullYear()) - person.year >= 19);
+	console.log(allAdults);
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 
+	const comment = comments.find(function(comment) {
+		if(comment.id === 823423) {
+			return true
+		}
+	});
+	console.log(comment);
+
 // Array.prototype.findIndex()
 // Find the comments with this ID
 // delete the comment with the ID of 823423
+
+	const index = comments.findIndex(comment => comment.id === 823423);
+
+	//console.log(index);
+
+	const newComments = [
+		...comments.slice(0, index),
+		...comments.slice(index, +1)
+	];
+
+	console.log(newComments);
